ab4bc15ce488feb002b3c530be042464617b1a74
Alexander Strakhovenko <sasstr@gmail.com>
2018-08-08 23:19:52 +0300

    Доделал разметку страниц мобильной версии и поправил gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -28,5 +28,5 @@ gulp.task("serve", function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
+  gulp.watch("source/*.html").on("change", server.reload);
 });
